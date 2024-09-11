6cfeeee478c1eb06f79c668e6e051e7b5465d51f
Dennis Kirk Smith Jr <j.r.@MacBook-Pro-2.local>
2024-09-11 15:59:16 -0400

    Started Verity Project With The Homie

---
+++ Verity.java
@@ -0,0 +1,11 @@
+public class Verity {
+    public String circle = "C";
+    public String square = "S";
+    public String triangle = "T";
+    public String leftInside = "";
+    public String middleInside = "";
+    public String rightInside = "";
+    public String leftOutside = "";
+    public String middleOutside = "";
+    public String rightOutside = "";
+}
